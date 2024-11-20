5aa6298e73fb6e64b824fe647853077985669dc1
saman-com <39319461+saman-com@users.noreply.github.com>
2024-11-21 10:24:56 +1300

    create security group

--- main.tf
+++ main.tf
@@ -19,19 +19,20 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
-  vpc_security_groups_ids = [ aws_security_group.blog.id]
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
 resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = " Allow http and https in. Allow everything out"
-
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -41,9 +42,9 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-
-  aws_security_group = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
+
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
@@ -51,9 +52,9 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-
-  aws_security_group = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
+
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
@@ -61,6 +62,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-
-  aws_security_group = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
