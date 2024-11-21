62ee4a7451f2ca7edafaf711e47053074de6d2ed
saman-com <39319461+saman-com@users.noreply.github.com>
2024-11-21 14:59:08 +1300

    auto scalaing

--- main.tf
+++ main.tf
@@ -75,6 +75,21 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.0"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
