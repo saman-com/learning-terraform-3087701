bca6bc6ba949fe1b2d4d0604a0276e6a4089be3e
saman-com <39319461+saman-com@users.noreply.github.com>
2024-11-21 14:52:52 +1300

    lb2

--- main.tf
+++ main.tf
@@ -23,26 +23,11 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
-}
-
-
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name = "blog"
-
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -79,9 +64,20 @@ module "blog_alb" {
   }
 }
 
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
+  version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
